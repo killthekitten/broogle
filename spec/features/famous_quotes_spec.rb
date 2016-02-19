4e1b38825b6f3d63a052a01eba014af3c0d67e2e
Nikolay Shebanov <nikolay.shebanov@gmail.com>
2016-02-19 16:13:15 +0300

    Covered the process of submitting quotes with specs

--- spec/features/famous_quotes_spec.rb
+++ spec/features/famous_quotes_spec.rb
@@ -30,6 +30,44 @@ RSpec.describe "Famous Quotes" do
     end
   end
 
+  describe "Submitting Quotes" do
+    let(:text) do
+      "TV and the Internet are good because they keep stupid people from spending too much time out in public."
+    end
+
+    before do
+      visit root_path
+      click_on "+ Add new quote"
+    end
+
+    it "allows to create a quote" do
+      fill_in "Author", with: "Douglas Coupland"
+      fill_in "Content", with: text
+      expect { click_on "Submit Quote" }.to change(FamousQuote, :count)
+    end
+
+    it "validates content presence" do
+      fill_in "Author", with: "Douglas Coupland"
+      fill_in "Content", with: nil
+      expect { click_on "Submit Quote" }.not_to change(FamousQuote, :count)
+      expect(page).to have_text "can't be blank"
+    end
+
+    it "allows empty author" do
+      fill_in "Author", with: nil
+      fill_in "Content", with: text
+      expect { click_on "Submit Quote" }.to change(FamousQuote, :count)
+    end
+
+    it "makes new quote searchable right away" do
+      fill_in "Content", with: text
+      click_on "Submit Quote"
+
+      search_for("tv internet")
+      expect(page).to have_text("stupid people")
+    end
+  end
+
   def search_for(query)
     fill_in :q, with: query
     click_on "Broogle it!"
